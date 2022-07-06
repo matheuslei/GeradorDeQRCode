dbcdc591d122e3210c92954258973ece22560196
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2022-07-06 10:04:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateQR() {
+  document.querySelector("#qr-image").style.display = "block";
+  let QRtext = document.querySelector("#text").value;
+  if (QRtext.trim().length == 0) {
+    document.querySelector("#qr-image .error").innerHTML =
+      "Por favor, digite um texto!";
+    document.querySelector("#img").style.display = "none";
+  } else {
+    document.querySelector("#img").style.display = "block";
+    document.querySelector("#qr-image .error").innerHTML = "";
+    document.querySelector("#img").src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=240x240&data=" + QRtext;
+  }
+}
